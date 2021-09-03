bf67da2e8f3a7269731dee8da8c2ef2fdd94a726
Aron Green <aronlgreen@gmail.com>
2021-09-03 02:19:40 +0000

    Implement algorithm

--- sequence.py
+++ sequence.py
@@ -1,8 +1,19 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
-# num_1 = 0
-# num_2 = 1
-# num_3 = 2
-# count = 0
+num_1 = 0
+num_2 = 1
+num_3 = 2
+count = 0
 
-# sum = num_1 + num_2 + num_3
+sum = num_1 + num_2 + num_3
+
+while count < n:
+    sum = num_1 + num_2 + num_3
+
+    num_1 = num_2
+    num_2 = num_3
+    num_3 = sum
+
+    count += 1
+
+    print(num_1)
